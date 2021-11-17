0d4225e3e2981a0544ac6e37b15d1d8c0bb79099
OAndrs <stadler500@freemail.hu>
2021-11-17 16:48:10 +0100

    difficulty

--- main.c
+++ main.c
@@ -24,231 +24,142 @@ SegmentLCD_LowerCharSegments_TypeDef lowerCharSegments[SEGMENT_LCD_NUM_OF_LOWER_
 #define PB0_PORT    gpioPortB
 #define PB0_PIN     9
 
+#define PB1_PORT    gpioPortB
+#define PB1_PIN     10
+
+#define SEG_NUM		4
 struct coordinate{
 	uint8_t x;
 	uint8_t y;
 }coordinate;
 
 bool shoot;
+bool gameStart = true;
 uint8_t duckCount = 0;
 uint8_t duckHits = 0;
 struct coordinate bullet;
 struct coordinate hunter;
 struct coordinate duck;
 
-void delay() {
-   for(int d=0;d<90000;d++);
-}
-void lcdTest1 (){
-	uint8_t i;
-	for(i=0;i<4;i++){
-		upperCharSegments[i].raw=1;
-	}
-}
-void lcdTest2(){
-	uint8_t i;
-	for(i=0;i<4;i++){
-			upperCharSegments[i].raw=0;
-		}
+void delay(int lim) {
+   for(int d=0;d < lim;d++);
 }
 void upperLcdUpdate (uint8_t DucksQty, uint8_t HitsQty){
+	SegmentLCD_Symbol(LCD_SYMBOL_COL10, 1);
+
 	uint8_t segments[4], i;
 	segments[3]=DucksQty/10;
 	segments[2]=DucksQty%10;
 	segments[1]=HitsQty/10;
 	segments[0]=HitsQty%10;
-
-	/*for(i=0;i<4;i++){
-		upperCharSegments[i].raw=1;
-	}*/
 	for(i=0;i<4;i++){
 			switch(segments[i]){
 			case 0:
 				if(i%2==0){
-					upperCharSegments[i].a=1;
+					upperCharSegments[i].raw=63;
+					/*upperCharSegments[i].a=1;
 					upperCharSegments[i].b=1;
 					upperCharSegments[i].c=1;
 					upperCharSegments[i].d=1;
 					upperCharSegments[i].e=1;
 					upperCharSegments[i].f=1;
-					upperCharSegments[i].g=0;
+					upperCharSegments[i].g=0;*/
 				}
 				else{
 					upperCharSegments[i].raw=0;
 				}
 				break;
 			case 1:
-				upperCharSegments[i].a=0;
+				upperCharSegments[i].raw=6;
+				/*upperCharSegments[i].a=0;
 				upperCharSegments[i].b=1;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=0;
 				upperCharSegments[i].e=0;
 				upperCharSegments[i].f=0;
-				upperCharSegments[i].g=0;
+				upperCharSegments[i].g=0;*/
 			    break;
 			case 2:
-				upperCharSegments[i].a=1;
+				upperCharSegments[i].raw=91;
+				/*upperCharSegments[i].a=1;
 				upperCharSegments[i].b=1;
 				upperCharSegments[i].c=0;
 				upperCharSegments[i].d=1;
 				upperCharSegments[i].e=1;
 				upperCharSegments[i].f=0;
-				upperCharSegments[i].g=1;
+				upperCharSegments[i].g=1;*/
 				break;
 			case 3:
-				upperCharSegments[i].a=1;
+				upperCharSegments[i].raw=79;
+				/*upperCharSegments[i].a=1;
 				upperCharSegments[i].b=1;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=1;
 				upperCharSegments[i].e=0;
 				upperCharSegments[i].f=0;
-				upperCharSegments[i].g=1;
+				upperCharSegments[i].g=1;*/
 				break;
 			case 4:
-				upperCharSegments[i].a=0;
+				upperCharSegments[i].raw=102;
+				/*upperCharSegments[i].a=0;
 				upperCharSegments[i].b=1;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=0;
 				upperCharSegments[i].e=0;
 				upperCharSegments[i].f=1;
-				upperCharSegments[i].g=1;
+				upperCharSegments[i].g=1;*/
 				break;
 			case 5:
-				upperCharSegments[i].a=1;
+				upperCharSegments[i].raw=109;
+				/*upperCharSegments[i].a=1;
 				upperCharSegments[i].b=0;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=1;
 				upperCharSegments[i].e=0;
 				upperCharSegments[i].f=1;
-				upperCharSegments[i].g=1;
+				upperCharSegments[i].g=1;*/
 				break;
 			case 6:
-				upperCharSegments[i].a=1;
+				upperCharSegments[i].raw=125;
+				/*upperCharSegments[i].a=1;
 				upperCharSegments[i].b=0;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=1;
 				upperCharSegments[i].e=1;
 				upperCharSegments[i].f=1;
-				upperCharSegments[i].g=1;
+				upperCharSegments[i].g=1;*/
 				break;
 			case 7:
-				upperCharSegments[i].a=1;
+				upperCharSegments[i].raw=7;
+				/*upperCharSegments[i].a=1;
 				upperCharSegments[i].b=1;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=0;
 				upperCharSegments[i].e=0;
 				upperCharSegments[i].f=0;
-				upperCharSegments[i].g=0;
+				upperCharSegments[i].g=0;*/
 				break;
 			case 8:
-				upperCharSegments[i].a=1;
+				upperCharSegments[i].raw=127;
+				/*upperCharSegments[i].a=1;
 				upperCharSegments[i].b=1;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=1;
 				upperCharSegments[i].e=1;
 				upperCharSegments[i].f=1;
-				upperCharSegments[i].g=1;
+				upperCharSegments[i].g=1;*/
 				break;
 			case 9:
-				upperCharSegments[i].a=1;
+				upperCharSegments[i].raw=111;
+				/*upperCharSegments[i].a=1;
 				upperCharSegments[i].b=1;
 				upperCharSegments[i].c=1;
 				upperCharSegments[i].d=1;
 				upperCharSegments[i].e=0;
 				upperCharSegments[i].f=1;
-				upperCharSegments[i].g=1;
+				upperCharSegments[i].g=1;*/
 				break;
 			}
-<<<<<<< Updated upstream
-=======
-			else{
-				upperCharSegments[i].raw=0;
-			}
-			break;
-		case 1:
-			upperCharSegments[i].a=0;
-			//upperCharSegments[i].b=1;
-			//upperCharSegments[i].c=1;
-			upperCharSegments[i].d=0;
-			upperCharSegments[i].e=0;
-			upperCharSegments[i].f=0;
-			upperCharSegments[i].g=0;
-		    break;
-		case 2:
-			//upperCharSegments[i].a=1;
-			//upperCharSegments[i].b=1;
-			upperCharSegments[i].c=0;
-			//upperCharSegments[i].d=1;
-			//upperCharSegments[i].e=1;
-			upperCharSegments[i].f=0;
-			//upperCharSegments[i].g=1;
-			break;
-		case 3:
-			//upperCharSegments[i].a=1;
-			//upperCharSegments[i].b=1;
-			//upperCharSegments[i].c=1;
-			//upperCharSegments[i].d=1;
-			upperCharSegments[i].e=0;
-			upperCharSegments[i].f=0;
-			//upperCharSegments[i].g=1;
-			break;
-		case 4:
-			upperCharSegments[i].a=0;
-			//upperCharSegments[i].b=1;
-			upperCharSegments[i].c=0;
-			upperCharSegments[i].d=0;
-			//upperCharSegments[i].e=1;
-			//upperCharSegments[i].f=1;
-			//upperCharSegments[i].g=1;
-			break;
-		case 5:
-			//upperCharSegments[i].a=1;
-			upperCharSegments[i].b=0;
-			//upperCharSegments[i].c=1;
-			//upperCharSegments[i].d=1;
-			upperCharSegments[i].e=0;
-			//upperCharSegments[i].f=1;
-			//upperCharSegments[i].g=1;
-			break;
-		case 6:
-			//upperCharSegments[i].a=1;
-			upperCharSegments[i].b=0;
-			//upperCharSegments[i].c=1;
-			//upperCharSegments[i].d=1;
-			//upperCharSegments[i].e=1;
-			//upperCharSegments[i].f=1;
-			//upperCharSegments[i].g=1;
-			break;
-		case 7:
-			//upperCharSegments[i].a=1;
-			//upperCharSegments[i].b=1;
-			//upperCharSegments[i].c=1;
-			upperCharSegments[i].d=0;
-			upperCharSegments[i].e=0;
-			upperCharSegments[i].f=0;
-			upperCharSegments[i].g=0;
-			break;
-		case 8:
-			//upperCharSegments[i].a=1;
-			//upperCharSegments[i].b=1;
-			//upperCharSegments[i].c=1;
-			//upperCharSegments[i].d=1;
-			//upperCharSegments[i].e=1;
-			//upperCharSegments[i].f=1;
-			//upperCharSegments[i].g=1;
-			break;
-		case 9:
-			//upperCharSegments[i].a=1;
-			//upperCharSegments[i].b=1;
-			//upperCharSegments[i].c=1;
-			//upperCharSegments[i].d=1;
-			upperCharSegments[i].e=0;
-			//upperCharSegments[i].f=1;
-			//upperCharSegments[i].g=1;
-			break;
-		}
-
->>>>>>> Stashed changes
 	}
 	SegmentLCD_UpperSegments(upperCharSegments);
 }
@@ -295,10 +206,17 @@ void lowerLcdUpdate(struct coordinate Duck, struct coordinate Bullet,struct coor
 	}
 	SegmentLCD_LowerSegments(lowerCharSegments);
 }
-void lcdDifficulty(uint8_t difficulty){
+void clearLowerLcd(){
+	uint8_t i;
+	for(i=0;i<4;i++){
+		lowerCharSegments[i].raw = 0;
+	}
+	SegmentLCD_LowerSegments(lowerCharSegments);
+}
+void lcdDifficulty(int difficulty){
 	uint8_t i;
 	for(i=0;i<8;i++){
-		if(i<difficulty){
+		if(i<=difficulty){
 			SegmentLCD_ARing(i, 1);
 		}
 		else{
@@ -309,16 +227,16 @@ void lcdDifficulty(uint8_t difficulty){
 void lcdDuckHit(struct coordinate Duck){
 	uint8_t i;
 	for(i=0;i<3;i++){
-		lowerCharSegments[i].a = 0;
+		lowerCharSegments[Duck.x].a = 0;
 		SegmentLCD_LowerSegments(lowerCharSegments);
-		delay();
-		lowerCharSegments[i].a = 1;
+		delay(200000);
+		lowerCharSegments[Duck.x].a = 1;
 		SegmentLCD_LowerSegments(lowerCharSegments);
-		delay();
+		delay(200000);
 	}
 	lowerCharSegments[i].a = 0;
 	SegmentLCD_LowerSegments(lowerCharSegments);
-	delay();
+	delay(100000);
 }
 void duckNewPosition(){
 	uint8_t newPosition;
@@ -335,14 +253,22 @@ void hunterPosition()
 	//0-48
 	uint32_t loc = CAPLESENSE_getSliderPosition();
 
-	if(loc >= 0 && loc <= 12)
-		hunter.x = 0;
-	else if(loc > 12 && loc <= 24)
-		hunter.x = 1;
-	else if(loc > 24 && loc <= 36)
-		hunter.x = 2;
-	else if(loc > 36 && loc <= 48)
-		hunter.x = 3;
+
+	for(uint8_t i = 0; i < 4; i++){
+		if(loc >= 48 * i / 4 &&loc <= 48 * (i + 1) / 4){
+			hunter.x = i;
+		}
+	}
+}
+int difficultyLevel(uint32_t loc)
+{
+
+	for(uint8_t i = 0; i < 8; i++){
+		if(loc >= 48 * i / 8 &&loc <= 48 * (i + 1) / 8){
+			return i;
+		}
+	}
+	return -1;
 }
 void TIMER1_IRQHandler()
 {
@@ -356,6 +282,7 @@ void GPIO_IRQHandler(void)
   shoot = true;
   bullet.x = hunter.x;
   GPIO_IntClear(1 << PB0_PIN);
+  GPIO_IntClear(1 << PB1_PIN);
 }
 
 // Interrupt Service Routine for even GPIO pins
@@ -382,6 +309,7 @@ int main() {
   CMU_ClockEnable(cmuClock_GPIO, true);
 
   GPIO_PinModeSet(PB0_PORT, PB0_PIN, gpioModeInput, 1);
+  GPIO_PinModeSet(PB1_PORT, PB1_PIN, gpioModeInput, 1);
 
   TIMER_Init_TypeDef timerInit =            // Setup Timer initialization
   {
@@ -399,7 +327,6 @@ int main() {
   };
 
   TIMER_IntEnable(TIMER1, TIMER_IF_OF);     // Enable Timer1 overflow interrupt
-  TIMER_TopSet(TIMER1, 15625);
 
   NVIC_EnableIRQ(TIMER1_IRQn);
   NVIC_EnableIRQ(GPIO_ODD_IRQn);
@@ -412,6 +339,7 @@ int main() {
   TIMER_Init(TIMER0, &timerInit);           // Configure and start Timer0
   TIMER_Init(TIMER1, &timerInit);           // Configure and start Timer1
   GPIO_IntConfig(PB0_PORT, PB0_PIN, true, false, true);
+  GPIO_IntConfig(PB1_PORT, PB1_PIN, true, false, true);
 
 
   CAPLESENSE_Init(false);
@@ -426,30 +354,58 @@ int main() {
 
   shoot = false;
 
-  SegmentLCD_Symbol(LCD_SYMBOL_COL10, 1);
 
-  while(1)
-  {
 
-	  if(shoot){
-		  if(bullet.y == 3){
-			  if(bullet.x == duck.x){
-				  duckHits++;
-				  duckNewPosition();
-				  TIMER_CounterSet(TIMER1, 0);
-			  }
-			  bullet.y = 0;
-			  shoot = false;
-		  }
-		  else{
-			  bullet.y += 1;
-			  delay();
-		  }
+
+  delay(100000);
+
+  int diff = 0;
+  while(1){
+	  int a = CAPLESENSE_getSliderPosition();
+	  if(a >= 0 && a <= 48){
+		  diff = difficultyLevel(a);
 	  }
+   lcdDifficulty(diff);
+   gameStart = false;
+   if(shoot){
+	   gameStart = true;
+	   shoot = false;
+	   TIMER_TopSet(TIMER1,12000 - diff * 1000);
+	   TIMER_CounterSet(TIMER1, 0);
+   }
 
-	  lowerLcdUpdate(duck, bullet, hunter);
-	  upperLcdUpdate(duckCount, duckHits);
+	  while(duckCount < 25 && gameStart )
+	  {
 
-	  hunterPosition();
+		  if(shoot){
+			  if(bullet.y == 3){
+				  if(bullet.x == duck.x){
+					  duckHits++;
+					  TIMER_CounterSet(TIMER1, 0); //Ne generalogjon veletlenul se kacsa a hatterben
+					  lcdDuckHit(duck);
+					  duckNewPosition();
+					  TIMER_CounterSet(TIMER1, 0);
+				  }
+				  bullet.y = 0;
+				  shoot = false;
+			  }
+			  else{
+				  bullet.y += 1;
+				  delay(100000);
+			  }
+		  }
+
+		  lowerLcdUpdate(duck, bullet, hunter);
+		  upperLcdUpdate(duckCount, duckHits);
+
+		  hunterPosition();
+
+		  //lcdDifficulty(4);
+		  delay(100000);
+	  }
+      clearLowerLcd();
+	  duckCount = 0;
+	  duckHits = 0;
+	  gameStart = false;
   }
 }
